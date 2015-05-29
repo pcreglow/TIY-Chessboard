45bd1666f5367799d338a2f798ff39236a02dced
Phillip Creglow <phillipcreglow@msn.com>
2015-05-29 15:21:58 -0400

    Setting up test/index.html

--- Scripts/models.js
+++ Scripts/models.js
@@ -1,43 +1,51 @@
-(function(window){
+(function(window) {
 
-  var initBoard = (function() {
+  window.chess = {
 
-    var black = { };
-
-    var white = { };
-
-    return black;
-    return white;
-    };
-
-  })();
-
-  var move = function(board, fromX, fromY, toX, toY) {
-
-
-  }
-
-  window.board = "";
-
-  var moves = [
-    /*board[6][3] = board[4][3];
-    board[6][3] = ' ';
-    board[0][6] = board[2][5];
-    board[0][6] = ' ';
-    board[6][2] = board[4][2];
-    board[6][2] = ' ';
-    board[1][4] = board[2][4];
-    board[1][4] = ' ';
-    board[6][6] = board[5][6];
-    board[6][6] = ' ';
-    board[1][3] = board[3][3];
-    board[1][3] = ' ';
-    board[7][5] = board[6][6];
-    board[7][5] = ' ';
-    board[0][5] = board[1][4];
-    board[0][5] = ' ';
-    board[7][7] = board[5][5];
-    board[6][6] = ' ';*/
-  ];
+  };
 
 })(window);
+
+
+/*var initBoard = (function(window) {
+
+  var black = { };
+
+  var white = { };
+
+  return black;
+  return white;
+  };
+
+})();
+
+var move = function(board, fromX, fromY, toX, toY) {
+
+
+}
+
+window.board = "";
+
+var moves = [
+  /*board[6][3] = board[4][3];
+  board[6][3] = ' ';
+  board[0][6] = board[2][5];
+  board[0][6] = ' ';
+  board[6][2] = board[4][2];
+  board[6][2] = ' ';
+  board[1][4] = board[2][4];
+  board[1][4] = ' ';
+  board[6][6] = board[5][6];
+  board[6][6] = ' ';
+  board[1][3] = board[3][3];
+  board[1][3] = ' ';
+  board[7][5] = board[6][6];
+  board[7][5] = ' ';
+  board[0][5] = board[1][4];
+  board[0][5] = ' ';
+  board[7][7] = board[5][5];
+  board[6][6] = ' ';*/
+/*];
+
+})(window);
+*/
